53e05539cd4e21eb00de52dde43bdf91a273bc48
zgaz <jack1102ng@gmail.com>
2022-12-03 08:53:45 +0700

    save change

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 const listItemNavbar = document.querySelectorAll('.header__list-item');
+const locationBtn = document.querySelector('.header-search__location');
+const locationFlexible = document.querySelector('.location-flexible');
 
-listItemNavbar.forEach(function(item) {
-    item.addEventListener('click', function(e) {
+
+locationBtn.addEventListener('click', (e) => {
+    locationFlexible.classList.toggle('show');
+})
+
+listItemNavbar.forEach(function (item) {
+    item.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('.header__list-item.header__list-item-link--active').classList.remove('header__list-item-link--active');
         this.classList.add('header__list-item-link--active');
@@ -11,31 +18,28 @@ listItemNavbar.forEach(function(item) {
 const headerMenubtn = document.querySelector('.header__setup-menu');
 const headerMenulist = document.querySelector('.header__setup-menu-list');
 
-headerMenubtn.addEventListener('click', function() {
+headerMenubtn.addEventListener('click', function () {
     headerMenulist.classList.toggle('show');
-
-    if(headerMenulist.classList.contains('show')) {
-        const btnList = headerMenulist.querySelectorAll('.header__setup-menu-verify .btn');
-    
-        btnList.forEach(function(btn) {
-            btn.addEventListener('click', function(e) {
-                modal.classList.add('show');
-            })
-        })
-    }
 })
 
 const modal = document.getElementById('myModal');
 const closeBtn = document.querySelector('.close');
 const modalContent = document.querySelector('.modal-content');
 
+const btnList = headerMenulist.querySelectorAll('.header__setup-menu-verify .btn');
 
-closeBtn.addEventListener('click', function() {
+btnList.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        modal.classList.add('show');
+    })
+})
+
+closeBtn.addEventListener('click', function () {
     modal.classList.remove('show');
 })
 
-modal.addEventListener('click', function(e) {
-    if(e.target === e.currentTarget) {
+modal.addEventListener('click', function (e) {
+    if (e.target === e.currentTarget) {
         modal.classList.remove('show');
     }
 })
@@ -44,23 +48,19 @@ modal.addEventListener('click', function(e) {
 var header = document.getElementById('header');
 var searchListBtn = document.querySelector('.header__list-search');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var scrollHeight = window.pageYOffset;
-    if(scrollHeight > 80) {
+    if (scrollHeight > 80) {
         header.classList.add('scroll');
     } else {
         header.classList.remove('scroll');
-        header.classList.remove('show');
     }
 })
-searchListBtn.addEventListener('click', function() {
+searchListBtn.addEventListener('click', function () {
     header.classList.add('show');
-    if(header.classList.contains('show')) {
-        window.addEventListener('scroll', function() {
-            var scrollHeight = window.pageYOffset;
-            if(scrollHeight > 80) {
-                header.classList.remove('show');
-            }
+    if (header.classList.contains('show')) {
+        window.addEventListener('scroll', function () {
+            header.classList.remove('show');
         })
     }
 })
